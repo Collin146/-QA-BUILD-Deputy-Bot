3ac991ed2c4674afae312e7ac666561357b3b4e5
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-16 15:06:20 +0200

    Created priority.js QA-1

---
+++ commands/priority.js
@@ -0,0 +1,88 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => { 
+
+    function catchErr (err, message) {
+
+        let errchannel = bot.channels.find(x => x.name === 'errors');
+        const warningsign = bot.emojis.get("729725849343098900");
+        
+        errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`prefix.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+        
+        }
+
+    //try {
+
+    const yes = bot.emojis.get("700713527576625205");
+    const no = bot.emojis.get("700713478578634783"); 
+    const warningsign = bot.emojis.get("729725849343098900");
+
+    message.channel.bulkDelete(1);
+
+    let priorityEmbed = new Discord.RichEmbed()
+    .setTitle(`${warningsign} **Priority Active!**`)
+    .setTimestamp()
+    .setColor("YELLOW")
+    .setDescription(`A priority has been started by ${message.author}. To all civilians, please refrain from creating any other priorities until this priority & the cooldown have ended! To end the priority, press the ${no} below. This can not be undone!`);
+  
+    message.channel.send(priorityEmbed);
+
+    message.react(no.id);
+
+    message.delete().catch(O_o=>{});
+
+    const filter = (reaction, user) => {
+	return [no.id].includes(reaction.emoji.id) && user.id === message.author.id;
+       };
+
+    message.awaitReactions(filter, { max: 1 })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if(reaction.emoji.id === no.id) {
+
+        message.channel.bulkDelete(1);
+
+        let priorityendEmbed = new Discord.RichEmbed()
+        .setTitle(`${warningsign} **Priorty Ended!**`)
+        .setTimestamp()
+        .setColor("GREEN")
+        .setDescription(`The previous priority that was created by ${message.author} has ended! Please wait for the 20 minute cooldown to end before creating another priority!`);
+
+        message.channel.send(priorityendEmbed);
+
+        var timeout = setTimeout(function(){
+
+                setInterval(function() {
+                    message.channel.send('Time left: '+getTimeLeft(timeout)+'s');
+                }, 5000);
+                
+                function getTimeLeft(timeout) {
+                    return Math.ceil((timeout._idleStart + timeout._idleTimeout - Date.now()) / 1000);
+                }
+
+                let cooldownendEmbed = new Discord.RichEmbed()
+                .setTitle(`${warningsign} **Cooldown Ended!**`)
+                .setTimestamp()
+                .setColor("GREEN")
+                .setDescription(`The priority cooldown has ended! You are now authorized to create another priority. When doing so, please use the \`!priority\` command!`);
+        
+                message.channel.send(cooldownendEmbed);
+
+            }, ms("30m"));
+
+		}
+	});
+   
+    // } catch(err) {
+    //     catchErr(err)
+
+    // }    
+
+}
+
+module.exports.help = {
+    name: "priority"
+}
