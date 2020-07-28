42fd40937f4ff7f0fe7549b6f0495bb6fc5a0592
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-29 01:59:58 +0200

    Updated dm.js QA-9

--- commands/dm.js
+++ commands/dm.js
@@ -52,7 +52,7 @@ const filter = (reaction, user) => {
   return [yes.id, no.id].includes(reaction.emoji.id) && user.id === message.author.id;
 };
 
-sentMessage.awaitReactions(filter, { max: 2, time: 60000, errors: ['time'] })
+sentMessage.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
   .then(collected => {
       const reaction = collected.first();
 
