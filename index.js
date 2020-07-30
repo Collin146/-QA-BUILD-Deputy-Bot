f851d95285d88d75a53da488b39ac79c286462bb
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-31 00:44:03 +0200

    Updated index.js QA-6

--- index.js
+++ index.js
@@ -832,7 +832,10 @@ const antiSpam = new AntiSpam({
     // And many more options... See the documentation.
 });
  
-antiSpam.on("warnAdd", (member) => antiSpam.message.channel.bulkDelete(3));
+antiSpam.on("warnAdd", (member) => { 
+    member.lastMessage.channel.bulkDelete(3);
+
+});
 
 bot.on('message', (message) => antiSpam.message(message)); 
 
