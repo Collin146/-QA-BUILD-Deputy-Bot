906fa799940f8af6b3c1ac0aab4e7a0896da3099
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-07-31 16:02:15 +0200

    Updated index.js QA-28

--- index.js
+++ index.js
@@ -817,7 +817,8 @@ const antiSpam = new AntiSpam({
     warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
     kickThreshold: 3, // Amount of messages sent in a row that will cause a ban.
     banThreshold: 7, // Amount of messages sent in a row that will cause a ban.
-    maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam..
+    maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+    warnMessage: '{@user}', // Message that will be sent in chat upon warning a user.
     kickMessage: '**{user_tag}** has been kicked for spamming.', // Message that will be sent in chat upon kicking a user.
     banMessage: '**{user_tag}** has been banned for spamming.', // Message that will be sent in chat upon banning a user.
     maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
