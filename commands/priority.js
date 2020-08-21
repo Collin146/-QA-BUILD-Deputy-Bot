a81dcea5bf2c169fe4da4a498dbb2c632b34ad45
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-08-21 13:37:45 +0200

    Update priority.js QA-8

--- commands/priority.js
+++ commands/priority.js
@@ -32,7 +32,7 @@ module.exports.run = async (bot, message, args) => {
     var utcCutoff = moment.utc(now, 'HH:mm:ss');
 
     var displayCutoff = 
-    moment.tz(utcCutoff.format('HH:mm:ss'), 'HH:mm:ss', 'United_Kingdom/Birmingham');
+    moment.tz(utcCutoff.format('HH:mm:ss'), 'HH:mm:ss', 'Europe/Birmingham');
 
     let priorityEmbed = new Discord.RichEmbed()
     .setTitle(`${warningsign} **Priority Active!**`)
