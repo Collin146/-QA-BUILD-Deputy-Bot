401e1b832dd1d6150bb7978d83bc2bac1ae1a819
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-08-31 21:45:16 +0200

    Updated priority.js hotfix QA-2

--- commands/priority.js
+++ commands/priority.js
@@ -58,7 +58,7 @@ const filter = (reaction, user) => {
       ].join('\n'))
 
     if (gmember.hasPermission("ADMINISTRATOR") && gmember.id !== message.author.id) modlogchannel.send({embed: priocancel});
-    return [no.id].includes(reaction.emoji.id) && user.id === message.author.id, gmember.roles.some(role => role.name === 'Administrator'); 
+    return [no.id].includes(reaction.emoji.id) && user.id === message.author.id, gmember.roles.some(role => role.name === 'Administrator'), gmember.roles.some(role => role.name === 'Deputy Director'), gmember.roles.some(role => role.name === 'Director'); 
 };
 
 
