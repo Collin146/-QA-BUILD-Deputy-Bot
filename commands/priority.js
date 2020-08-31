38283f577f32dc973f17387358de443f3b7697d5
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-08-31 22:05:36 +0200

    Updated priority.js hotfix QA-5

--- commands/priority.js
+++ commands/priority.js
@@ -43,7 +43,7 @@ module.exports.run = async (bot, message, args) => {
       });
 
 const filter = (reaction, user) => {
-    gmember = message.guild.members.cache.get(user.id)
+    gmember = message.guild.members.fetch(user.id)
 
     let modlogchannel = message.guild.channels.find(x => x.name === 'modlog');
 
