aabfad3ce75a65a723daf3d106ceca5ab1fe0370
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-09-17 17:43:36 +0200

    Updated index.js selfrolescript QA-6

--- index.js
+++ index.js
@@ -1131,7 +1131,8 @@ bot.on("ready", async () => {
             .setTitle(`${no} **Verification Failed!**`)
             .setDescription("You do not appear to be a member of Deputy Roleplay. Please reach out to any available Administrator if you believe this is a mistake.");
       
-            let mainguild = bot.guilds.get('644227663829139466');
+            let mainguild = bot.guild.get('644227663829139466');
+            mainguild.members.fetch()
           
           if (mainguild.members.get(user.id)) {
             gMember.addRole(roledrpmember.id);
@@ -1147,7 +1148,8 @@ bot.on("ready", async () => {
             .setTitle(`${no} **Verification Failed!**`)
             .setDescription("You do not appear to be a recruit of Deputy Roleplay. Please reach out to any available Administrator if you believe this is a mistake.");
       
-            let trainguild = bot.guilds.get('645035452956540929');
+            let trainguild = bot.guild.get('645035452956540929');
+            trainguild.members.fetch()
           
             if (trainguild.members.get(user.id)) {
                 gMember.addRole(roledrprecruit.id);
@@ -1163,7 +1165,8 @@ bot.on("ready", async () => {
             .setTitle(`${no} **Verification Failed!**`)
             .setDescription("You do not appear to be an applicant of Deputy Roleplay. Please reach out to any available Administrator if you believe this is a mistake.");
       
-            let interviewguild = bot.guilds.get('604420918634086411');
+            let interviewguild = bot.guild.get('604420918634086411');
+            interviewguild.members.fetch()
           
           if (interviewguild.members.get(user.id)) {
             gMember.addRole(roledrpapplicant.id);
