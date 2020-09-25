4d0f8b31d248679b19ccbc1e3f6a17b3812fbaa0
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-09-26 00:54:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function catchErr (err, message) {
 let errchannel = bot.channels.find(x => x.name === 'errors');
 const warningsign = bot.emojis.get("729725849343098900");
 
-errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`index.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+// errchannel.send(`**<@292598566759956480> ${warningsign} Error Detected in \`index.js\` ${warningsign}** \`\`\`` + err + `\`\`\``);
+	console.log(err)
 
 }
 
