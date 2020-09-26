34dc4b85bae9b44a5c75e335c95f11aab70fc650
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-09-26 02:53:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1102,32 +1102,28 @@ bot.on("ready", async () => {
     //     limit: 80,
     //    });
 
-    // const drp1 = bot.emojis.get("759125897953017857");
-    // const drp2 = bot.emojis.get("759125936586883072");
-    // const drp3 = bot.emojis.get("759125984967393330");
-    // const drp4 = bot.emojis.get("759126011265941506");
-    // const drp5 = bot.emojis.get("759126035215810592");
-    // const drp6 = bot.emojis.get("759126060355813376");
-    // const drp7 = bot.emojis.get("759126083781394444");
+    const drp1 = bot.emojis.get("759125897953017857");
+    const drp2 = bot.emojis.get("759125936586883072");
+    const drp3 = bot.emojis.get("759125984967393330");
+    const drp4 = bot.emojis.get("759126011265941506");
+    const drp5 = bot.emojis.get("759126035215810592");
+    const drp6 = bot.emojis.get("759126060355813376");
+    const drp7 = bot.emojis.get("759126083781394444");
 
-    // fetchedMessage.reactions.resolve(drp1.id).users.fetch()
-    // fetchedMessage.reactions.resolve(drp2.id).users.fetch()
-    // fetchedMessage.reactions.resolve(drp3.id).users.fetch()
-    // fetchedMessage.reactions.resolve(drp4.id).users.fetch()
-    // fetchedMessage.reactions.resolve(drp5.id).users.fetch()
-    // fetchedMessage.reactions.resolve(drp6.id).users.fetch()
-    // fetchedMessage.reactions.resolve(drp7.id).users.fetch()
+    fetchedMessage.reactions.resolve(drp1.id).users.fetch()
+    fetchedMessage.reactions.resolve(drp2.id).users.fetch()
+    fetchedMessage.reactions.resolve(drp3.id).users.fetch()
+    fetchedMessage.reactions.resolve(drp4.id).users.fetch()
+    fetchedMessage.reactions.resolve(drp5.id).users.fetch()
+    fetchedMessage.reactions.resolve(drp6.id).users.fetch()
+    fetchedMessage.reactions.resolve(drp7.id).users.fetch()
 
     });
     
 
     bot.on('messageReactionAdd', async (messageReaction, user) => {
-    
-        try {
 
-                messageReaction.users.fetch({
-                    limit: 80,
-                   });
+        try {
         
         if (messageReaction.message.id === "759161531572813854") {
 
