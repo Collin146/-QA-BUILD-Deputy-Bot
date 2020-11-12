4b8bb223fa893d00ac096d4cd90c4449cf1d8b63
Collin146 <47307903+Collin146@users.noreply.github.com>
2020-11-12 02:37:55 +0100

    Updated accept.js dmEmbed

--- commands/accept.js
+++ commands/accept.js
@@ -30,6 +30,7 @@ module.exports.run = async (bot, message, args) => {
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783");  
 let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+let rDuser = bot.users.get(args[0]);
 
 let errEmbed = new Discord.RichEmbed()
       .setColor("RED")
@@ -63,6 +64,9 @@ let acceptembed = new Discord.RichEmbed()
           `**Date & Time:** ${moment.utc(message.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`,
           `**Status:** Approved`,
           `**Message:** Your application has been approved! You have taken your first step into the community. The next step is to wait for an interview to be announced/scheduled in <#644273618011815946>. We wish you good luck!`,
+          ` `,
+          `Please keep in mind that you will have a total of 30 days to attend an interview. If you fail to do so within those 30 days, your approved for interview status will unfortunately be removed and you will be forced to reapply if you want to be eligible for interviews again. To see how many days you have left, please refer to the following document: https://bit.ly/3kxyyr5`,
+          `(Please note that it may take several hours until you get added to this document).`
         ].join('\n'))
     
 rUser.send(acceptembed);
@@ -87,6 +91,23 @@ let ModEmbed = new Discord.RichEmbed()
 
 let modlogchannel = message.guild.channels.find(x => x.name === 'modlog');
 modlogchannel.send({embed: ModEmbed});
+
+let portalguild = bot.guilds.get('644301808680042506');
+
+let acceptedEmbed = new Discord.RichEmbed()
+.setTitle("**Applicant Accepted!**")
+.setTimestamp()
+.setColor("BLACK")
+.setDescription([
+    `**Date & Time:** ${moment.utc(message.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`,
+    `**Applicant Name:** ${rDuser.tag}`,
+    `**Applicant ID:** ${rUser.id}`,
+    `**Accepted By:** ${message.author.username}`,
+  ].join('\n'))
+.setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
+
+let acceptedlog = portalguild.channels.find(x => x.name === 'accepted-log');
+acceptedlog.send({embed: acceptedEmbed});
 
     } catch (err)  {
      catchErr(err);
