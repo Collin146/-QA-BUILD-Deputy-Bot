eb83e745b677ad08d23aa3a248e279833dc08677
Collin146 <47307903+Collin146@users.noreply.github.com>
2021-02-02 19:37:51 +0100

    Update aop.js

--- commands/aop.js
+++ commands/aop.js
@@ -32,7 +32,7 @@ let dmembed = new Discord.RichEmbed()
 .setDescription(`Per ${message.author}, AOP has been changed to Blaine County! Please finish your scenarios and head to the new AOP.`);
 
 message.guild.members.forEach(member => {
-    if (message.member.roles.find(r => r.name === "On Patrol")) member.send(dmembed);
+    if (member.roles.find(r => r.name === "On Patrol")) member.send(dmembed);
   });
 
   return; 
@@ -48,7 +48,7 @@ if(args[0] === "ss&s"){
     .setDescription(`Per ${message.author}, AOP has been changed to Sandy Shores & Surrounding! Please finish your scenarios and head to the new AOP.`);
     
     message.guild.members.forEach(member => {
-        if (message.member.roles.find(r => r.name === "On Patrol")) member.send(dmembed2);
+        if (member.roles.find(r => r.name === "On Patrol")) member.send(dmembed2);
       });
     
       return; 
