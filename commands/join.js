f042c7723da24c54154e2ed21355642c6781123b
Collin146 <47307903+Collin146@users.noreply.github.com>
2021-03-17 01:43:30 +0100

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -3,16 +3,12 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => { 
 
-  if (!message.guild) return;
+      let RTOchannel = message.guild.channels.find(x => x.id === '700641419257184297');
+      
+RTOchannel.voiceChannel.join();
 
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      const connection = await message.member.voiceChannel.join();
-    } else {
-      message.reply('You need to join a voice channel first!');
     }
 
-  
 }
 
 module.exports.help = {
