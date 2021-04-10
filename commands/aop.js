29b58207a8aa1c6eae83fbcfe936a075e64db1e7
Collin146 <47307903+Collin146@users.noreply.github.com>
2021-04-10 20:42:13 +0200

    QA01 aop.js

--- commands/aop.js
+++ commands/aop.js
@@ -12,17 +12,27 @@ function catchErr (err, message) {
         
         }
         
-//try {
+try {
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+    if(args[0] === "help"){
+        message.reply("Usage: !aop <bc, ss&s, ls, mt, or v&r>");
+        return;
+    }
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
+    if(args[0] == "help"){
+        message.reply("Usage: !aop <bc, ss&s, ls, mt, or v&r>");
+        return;
+    }
     
 const yes = bot.emojis.get("700713527576625205");
 const no = bot.emojis.get("700713478578634783"); 
 const warningsign = bot.emojis.get("729725849343098900");
 
-const mainguild = bot.guilds.get('700639523272523776')
+const mainguild = bot.guilds.get('644227663829139466')
 
-let AOPchannel = mainguild.channels.find(x => x.id === '806224054079324220');
-
-let patrolrole = mainguild.roles.find(x => x.name === 'On Patrol');
+let AOPchannel = mainguild.channels.find(x => x.id === '806204219321090069');
 
 message.delete().catch(_O_o=>{})
 
@@ -58,10 +68,58 @@ if(args[0] === "ss&s"){
       return;
     }
 
-// } catch(err) {
-//     console.log(err) 
+    if(args[0] === "ls"){
+
+        AOPchannel.edit({ name: 'Current AOP: LS' })
+        
+        let dmembed2 = new Discord.RichEmbed()
+        .setColor("RED")
+        .setTitle(`${warningsign} **Notice!**`)
+        .setDescription(`Per ${message.author}, AOP has been changed to Los Santos! Please finish your scenarios and head to the new AOP.`);
     
-//     }
+        message.guild.members.forEach(member => {
+            if (member.roles.has('708007528122024006') && !member.user.bot) member.send(dmembed2)
+          });
+        
+          return;
+        }
+
+        if(args[0] === "mt"){
+
+            AOPchannel.edit({ name: 'Current AOP: MT' })
+            
+            let dmembed2 = new Discord.RichEmbed()
+            .setColor("RED")
+            .setTitle(`${warningsign} **Notice!**`)
+            .setDescription(`Per ${message.author}, AOP has been changed to Metro! Please finish your scenarios and head to the new AOP.`);
+        
+            message.guild.members.forEach(member => {
+                if (member.roles.has('708007528122024006') && !member.user.bot) member.send(dmembed2)
+              });
+            
+              return;
+            }
+
+            if(args[0] === "v&r"){
+
+                AOPchannel.edit({ name: 'Current AOP: V&R' })
+                
+                let dmembed2 = new Discord.RichEmbed()
+                .setColor("RED")
+                .setTitle(`${warningsign} **Notice!**`)
+                .setDescription(`Per ${message.author}, AOP has been changed to Vinewood & Rockford! Please finish your scenarios and head to the new AOP.`);
+            
+                message.guild.members.forEach(member => {
+                    if (member.roles.has('708007528122024006') && !member.user.bot) member.send(dmembed2)
+                  });
+                
+                  return;
+                }
+
+ } catch(err) {
+     catchErr(err) 
+    
+     }
     
     }
 
